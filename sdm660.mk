195baec99b218c9ba5dcf3f0feadbe1c45bfc3c9
Ratoriku <a1063021545@gmail.com>
2021-10-21 22:19:11 +0800

    sdm660-common: Drop TetheringOverlay
    * It is forced to use bpf in Android S. Also we have it backported in k4.4, so this overlay is totally useless now
    
    Signed-off-by: Ratoriku <a1063021545@gmail.com>
    Signed-off-by: pix106 <sbordenave@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -577,7 +577,6 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
     hostapd \
     libwifi-hal-qcom \
-    TetheringConfigOverlay \
     WifiOverlay \
     wpa_supplicant \
     wpa_supplicant.conf \

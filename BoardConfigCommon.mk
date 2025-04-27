ceeb5b12248c5a961e8755e2a2212896ac4031ef
Michael Bestas <mkbestas@lineageos.org>
2025-04-28 02:21:39 +0300

    sdm660-common: Remove no longer used TARGET_USES_YCRCB_CAMERA_ENCODE

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -71,9 +71,6 @@ TARGET_NO_BOOTLOADER := true
 # Build Rules
 BUILD_BROKEN_DUP_RULES := true
 
-# Camera
-TARGET_USES_YCRCB_CAMERA_ENCODE := true
-
 # ConfigFS
 TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/configs/config.fs
 
